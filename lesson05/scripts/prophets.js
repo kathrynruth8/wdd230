85c3170398d813ac47dd0748bdd02e6ceee51ae8
Kathryn Lambert <lam20005@byui.edu>
2023-11-07 10:58:46 -0700

    prophets js and styling work

--- lesson05/scripts/prophets.js
+++ lesson05/scripts/prophets.js
@@ -14,11 +14,28 @@ function displayProphets(prophets) {
     // Create elements to add to the div.cards element
     const card = document.createElement('section');
     const fullName = document.createElement('h2');
+    const birthdate = document.createElement('p');
+    const placeOfBirth = document.createElement('p');
     const portrait = document.createElement('img');
-
+    // Build the h2 content out to show the prophet's full name
     fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+    birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+    placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
+    // Build the image portrait by setting all the relevant attributes
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute(
+      'alt',
+      `portrait of ${prophet.name} ${prophet.lastname}`
+    );
+    portrait.setAttribute('loading', 'lazy');
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
 
+    // Append the section(card) with the created elements
     card.appendChild(fullName);
+    card.appendChild(birthdate);
+    card.appendChild(placeOfBirth);
+    card.appendChild(portrait);
     cards.appendChild(card);
   });
 }
