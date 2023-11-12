7225cf654f3693bd4b2a2cb744e11a99dee60e82
Kathryn Lambert <lam20005@byui.edu>
2023-11-11 18:25:00 -0700

    testing

---
+++ testing/testing.js
@@ -0,0 +1,27 @@
+// select HTML elements in the document
+const url =
+  'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=7b0075e0d84601ad686fd9a9b87accbc&units=imperial';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
