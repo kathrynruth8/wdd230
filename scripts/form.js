d25d201c48da1292a6b52c31a866e83dc180a3aa
Kathryn Lambert <lam20005@byui.edu>
2023-11-13 19:38:39 -0700

    form work

--- scripts/form.js
+++ scripts/form.js
@@ -1,14 +1,6 @@
 const rangevalue = document.getElementById('rangevalue');
 const range = document.getElementById('r');
 
-const pass = document.querySelector('#password');
-const pass2 = document.querySelector('#password_retype');
-const message = document.querySelector('#message');
-
-const submit = document
-  .querySelector('#submitBtn')
-  .addEventListener('click', checkSame);
-
 // RANGE event listener
 range.addEventListener('change', displayRatingValue);
 range.addEventListener('input', displayRatingValue);
@@ -17,9 +9,17 @@ function displayRatingValue() {
   rangevalue.innerHTML = range.value;
 }
 
+const password = document.querySelector('#password');
+const retypePassword = document.querySelector('#password_retype');
+const message = document.querySelector('#message');
+
+const submit = document
+  .querySelector('.submitBtn')
+  .addEventListener('click', checkSame);
+
 // PASSWORD CHECK
 function checkSame() {
-  if (pass.value !== pass2.value) {
+  if (password.value !== retypePassword.value) {
     message.textContent = 'PASSWORDS DO NOT MATCH';
     message.style.visibility = 'show';
   } else {
