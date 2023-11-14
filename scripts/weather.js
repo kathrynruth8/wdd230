19b1052a47142cfdc0b01b448723d44f6c27330d
Kathryn Lambert <lam20005@byui.edu>
2023-11-14 09:27:56 -0700

    weather on wdd230 class page index

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,10 +1,11 @@
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
+const currentConditions = document.querySelector('#current-conditions');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#figcaption');
 
 const url =
-  'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=7b0075e0d84601ad686fd9a9b87accbc&units=imperial';
+  'https://api.openweathermap.org/data/2.5/weather?lat=43.82&lon=111.79&appid=7b0075e0d84601ad686fd9a9b87accbc&units=imperial';
 
 async function apiFetch() {
   try {
@@ -23,8 +24,9 @@ apiFetch();
 
 function displayResults(data) {
   currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  currentConditions.innerHTML = `${data.weather[0].main}`;
   const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  let desc = data.weather[0].main;
+  let desc = data.weather[0].description;
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = `${desc}`;
