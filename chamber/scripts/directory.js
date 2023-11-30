07c24fd2f31746518b96e04fe4e82442bb41f7a9
Kathryn Lambert <lam20005@byui.edu>
2023-11-30 13:10:38 -0700

    commit for new dan gould image

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -4,8 +4,8 @@ const cards = document.querySelector('#directory-cards');
 async function getMemberData() {
   const response = await fetch(url);
   const data = await response.json();
-  console.table(data.members);
-  // displayMembers(data.members);
+  // console.table(data.members);
+  displayMembers(data.members);
 }
 
 function displayMembers(members) {
@@ -13,19 +13,37 @@ function displayMembers(members) {
     // Create elements to add to the div.cards element
     const card = document.createElement('section');
     const name = document.createElement('h2');
-    const address = document.createElement('p');
+    // const address = document.createElement('p');
     const phone = document.createElement('p');
     const website = document.createElement('p');
     const description = document.createElement('p');
     const logo = document.createElement('img');
-    const { street1, city, state, zipcode } = member.address;
+    card.appendChild(name);
+
+    const address = member.address;
+    address.forEach((addressLine) => {
+      const address = document.createElement('p');
+      address.textContent = `${addressLine.line1} ${addressLine.city}, ${addressLine.state} ${addressLine.zipcode}`;
+      card.appendChild(address);
+    });
     // Build the text content
     name.textContent = member.name;
-    address.textContent = `${street1} ${city}, ${state} ${zipcode}`;
+    address.textContent = `${member.address}`;
     phone.textContent = member.phone;
     website.textContent = member.url;
     description.textContent = member.description;
-    logo.setAttribute('src', member.image - url);
+    logo.setAttribute('src', member.imageurl);
+    logo.setAttribute('alt', `logo for ${member.name}`);
+    logo.setAttribute('loading', 'lazy');
+    logo.setAttribute('width', '340');
+    logo.setAttribute('height', 'auto');
+    // Append the section(card) with the created elements
+
+    card.appendChild(phone);
+    card.appendChild(website);
+    card.appendChild(description);
+    card.appendChild(logo);
+    cards.appendChild(card);
   });
 }
 getMemberData();
