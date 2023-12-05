6c6f525e584402fd3ec90767bd12f7c1cde12b8d
Kathryn Lambert <lam20005@byui.edu>
2023-12-05 10:34:30 -0700

    discover page grid/list view

--- testing/testing.js
+++ testing/testing.js
@@ -1,27 +1,73 @@
-// select HTML elements in the document
-const url =
-  'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=7b0075e0d84601ad686fd9a9b87accbc&units=imperial';
+const url = 'https://kathrynruth8.github.io/wdd230/chamber/data/members.json';
+const cards = document.querySelector('#directory-cards');
 
-async function apiFetch() {
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      displayResults(data);
-    } else {
-      throw Error(await response.text());
-    }
-  } catch (error) {
-    console.log(error);
-  }
+async function getMemberData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.table(data.members);
+  displayMembers(data.members);
 }
-apiFetch();
 
-function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-  let desc = data.weather[0].description;
-  weatherIcon.setAttribute('src', iconsrc);
-  weatherIcon.setAttribute('alt', desc);
-  captionDesc.textContent = `${desc}`;
+function displayMembers(members) {
+  members.forEach((member) => {
+    // Create elements to add to the div.cards element
+    const card = document.createElement('section');
+    card.className = 'member-card';
+    const name = document.createElement('h2');
+    // const address = document.createElement('p');
+    const phone = document.createElement('p');
+    const description = document.createElement('p');
+    const websiteBox = document.createElement('p');
+    const website = document.createElement('a');
+    const logo = document.createElement('img');
+    card.appendChild(name);
+
+    const address = member.address;
+    address.forEach((addressLine) => {
+      const address = document.createElement('p');
+      address.textContent = `${addressLine.line1} ${addressLine.city}, ${addressLine.state} ${addressLine.zipcode}`;
+      card.appendChild(address);
+    });
+    // Build the text content
+    name.textContent = member.name;
+    address.textContent = `${member.address}`;
+    phone.textContent = member.phone;
+    description.textContent = member.description;
+    website.setAttribute('href', member.url);
+    websiteBox.id = 'directory-website';
+    websiteBox.appendChild(website);
+    website.textContent = 'Visit Us';
+    logo.setAttribute('src', member.imageurl);
+    logo.setAttribute('alt', `logo for ${member.name}`);
+    logo.setAttribute('loading', 'lazy');
+    logo.setAttribute('width', '250');
+    logo.setAttribute('height', 'auto');
+    // Append the section(card) with the created elements
+
+    card.appendChild(phone);
+    card.appendChild(description);
+    card.appendChild(websiteBox);
+    card.appendChild(logo);
+    cards.appendChild(card);
+  });
+}
+getMemberData();
+
+const gridbutton = document.querySelector('#grid');
+const listbutton = document.querySelector('#list');
+const display = document.querySelector('#directory-cards');
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener('click', () => {
+  // example using arrow function
+  display.classList.add('grid');
+  display.classList.remove('list');
+});
+
+listbutton.addEventListener('click', showList); // example using defined function
+
+function showList() {
+  display.classList.add('list');
+  display.classList.remove('grid');
 }
